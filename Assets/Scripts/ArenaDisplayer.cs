0199bc4b3a898e84945a1bea972b88aecce8c080
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-17 18:55:41 -0400

    Add ArenaDisplayer to Arena prefab

---
+++ Assets/Scripts/ArenaDisplayer.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaDisplayer : MonoBehaviour
+{
+    public List<CardHolderDisplayer> allyHolders;
+    public CardHolderDisplayer allyHand;
+    public List<CardHolderDisplayer> enemyHolders;
+    public CardHolderDisplayer enemyHand;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
