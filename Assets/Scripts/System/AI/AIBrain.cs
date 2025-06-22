5d2cfe839a3a77aae98a5ba04fcff5b8ea9f8b26
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-22 15:57:27 -0400

    Extract priority into AIBrain*
    *from AIRandom

--- Assets/Scripts/System/AI/AIBrain.cs
+++ Assets/Scripts/System/AI/AIBrain.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 //[CreateAssetMenu(menuName = "AIBrains/")]
 public abstract class AIBrain:ScriptableObject
 {
+    [Tooltip("Which move it should prioritize. Set to PASS to not prioritize anything")]
+    public Move.Type priority;
+
     public abstract Move pickMove(List<Move> moves);
 
     protected Move pickMoveRandom(List<Move> moves)
     {
         int index = UnityEngine.Random.Range(0, moves.Count);
         return moves[index];
+    }
+
+    protected Move pickMoveRandomPriority(List<Move> moves)
+    {
+        if (priority != Move.Type.PASS)
+        {
+            Move priorityMove = pickMoveFilter(moves, move => move.type == priority);
+            if (priorityMove.Valid)
+            {
+                return priorityMove;
+            }
+        }
+        return pickMoveRandomPriority(moves);
     }
 
     protected Move pickMoveFilter(List<Move> moves, Predicate<Move> filterFunc)
--- Assets/Scripts/System/AI/AIRandom.cs
+++ Assets/Scripts/System/AI/AIRandom.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="AIBrains/Random")]
 public class AIRandom : AIBrain
 {
-    [Tooltip("Which move it should prioritize. Set to PASS to not prioritize anything")]
-    public Move.Type priority;
 
     public override Move pickMove(List<Move> moves)
     {
-        if (priority != Move.Type.PASS)
-        {
-            Move priorityMove = pickMoveFilter(moves,move=>move.type == priority);
-            if (priorityMove.Valid)
-            {
-                return priorityMove;
-            }
-        }
-        return pickMoveRandom(moves);
+        return pickMoveRandomPriority(moves);
     }
 }
