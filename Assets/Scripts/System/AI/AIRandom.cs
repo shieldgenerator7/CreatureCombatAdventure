a0a4ecd762f1c4eb33949f4b785575042832e15d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-22 15:02:56 -0400

    Random AI can prioritize play moves

--- Assets/Scripts/System/AI/AIRandom.cs
+++ Assets/Scripts/System/AI/AIRandom.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="AIBrains/Random")]
 public class AIRandom : AIBrain
 {
+    public bool prioritizePlaying = true;
+
     public override Move pickMove(List<Move> moves)
     {
+        if (prioritizePlaying)
+        {
+            List<Move> playMoves = moves.FindAll(move => move.isMovePlay);
+            if (playMoves.Count > 0)
+            {
+                int playindex = Random.Range(0, playMoves.Count);
+                return playMoves[playindex];
+            }
+        }
         int index = Random.Range(0, moves.Count);
         return moves[index];
     }
