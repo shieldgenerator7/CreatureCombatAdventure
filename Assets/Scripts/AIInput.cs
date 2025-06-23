3c6941d9539872f22dd47231f5708a4204fd27e4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-23 01:33:29 -0400

    Refactor AI if statement about having valid moves

--- Assets/Scripts/AIInput.cs
+++ Assets/Scripts/AIInput.cs
@@ -12,8 +12,6 @@ public class AIInput : WranglerInput
     public override void processTurn()
     {
         base.processTurn();
-        //try playing cards
-        if (canPlayAnyCard()) {
             //get moves
             List<Move> moves = new List<Move>();
             controller.Wrangler.cardList.ForEach(card =>
@@ -25,19 +23,12 @@ public class AIInput : WranglerInput
                     moves.Add(new Move(card, holder));
                 });
             });
+        if (moves.Count == 0)
+        {
+            return;
+        }
             //ask ai to pick a move to do
             Move move = aiBrain.pickMove(moves);
             controller.Wrangler.placeCard(move.card, move.holder);
-        }//
-    }
-
-    private bool canPlayAnyCard()
-    {
-        return controller.Wrangler.cardList.Any(c =>
-            //can be played
-            controller.Wrangler.canPickupCard(c)
-            //can be placed at any position
-            && controller.Wrangler.cardHolders.Any(h => controller.Wrangler.canPlaceCardAt(c, h))
-            );
     }
 }
