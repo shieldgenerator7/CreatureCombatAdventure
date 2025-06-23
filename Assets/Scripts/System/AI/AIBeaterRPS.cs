e36e439670fb62a0258688d69953027519615aff
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-23 02:14:25 -0400

    Try to make RPS beater AI not move from a spot that it wins RPS at

--- Assets/Scripts/System/AI/AIBeaterRPS.cs
+++ Assets/Scripts/System/AI/AIBeaterRPS.cs
@@ -6,9 +6,14 @@ public class AIBeaterRPS : AIBrain
 {
     public override Move pickMove(List<Move> moves)
     {
-        Move filteredMove = pickMoveFilter(moves, move => move.beatsOpposingRPS);
-        return (filteredMove.Valid)
-            ? filteredMove
-            : pickMoveRandomPriority(moves);
+        Move filteredMove;
+        //Try 1: Move that beats RPS
+        filteredMove = pickMoveFilter(moves, move => move.beatsOpposingRPS);
+        if (filteredMove.Valid) { return filteredMove; }
+        //Try 2: Move that isn't currently beating RPS
+        filteredMove = pickMoveFilter(moves, move => !move.cardState.beatsOpposingRPS);
+        if (filteredMove.Valid) { return filteredMove; }
+        //Default: random move
+        return pickMoveRandomPriority(moves);
     }
 }
