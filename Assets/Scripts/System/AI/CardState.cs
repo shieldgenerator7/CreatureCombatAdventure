707e8704fa28a0605ac76f4bc39ab82ea31ade82
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-06-23 02:01:13 -0400

    Fill out CardState*
    *copied a lot from Move

--- Assets/Scripts/System/AI/CardState.cs
+++ Assets/Scripts/System/AI/CardState.cs
@@ -1,11 +1,49 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public struct CardState
 {
     public Card card;
+    public CardHolder holder;
+
+    public Card opposingCard;
+    public CardHolder opposingHolder;
+
+    /// <summary>
+    /// True if this card is causing this wrangler to win RPS in this lane
+    /// </summary>
+    public bool beatsOpposingRPS;
+    /// <summary>
+    /// True if this wrangler is winning power (raw) in this lane, and removing this card would cause that not to be true
+    /// </summary>
+    public bool beatsOpposingPowerRaw;
+
+    public bool isFirst;
+    public bool isLast;
 
     public CardState(Card card)
     {
+        if (!card)
+        {
+            throw new NullReferenceException($"card cannot be null! {card}");
+        }
+
         this.card = card;
+        holder = card.holder;
+
+        opposingHolder = holder.opposingHolder;
+        opposingCard = opposingHolder.Card;
+
+        isFirst = !holder.isHand && holder.Card == card;
+        isLast = !holder.isHand && holder.cardList.Last() == card;
+
+        beatsOpposingRPS = isFirst && holder.arena.data.rpsSetData.beats(
+            card.data.rps,
+            opposingCard?.data.rps ?? RockPaperScissors.NONE
+            );
+        int holderPower = holder.PowerRaw;
+        int holderPowerOpposing = opposingHolder.PowerRaw;
+        beatsOpposingPowerRaw = holderPower > holderPowerOpposing && holderPower - card.data.power <= holderPowerOpposing;
     }
 }
